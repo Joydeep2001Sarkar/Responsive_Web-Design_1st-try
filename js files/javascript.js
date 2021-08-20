798b85c4a43e01ed166bea68b9168c74eb377c6f
Joydeep <joydeepsarkar0026@gmail.com>
2021-08-20 19:02:39 +0530

    This is a change

---
+++ js files/javascript.js
@@ -0,0 +1,14 @@
+// To make Ham-burger button 
+
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navlist =  document.querySelector('.nav-list');
+rightnav = document.querySelector('.rightnav');
+
+burger.addEventListener('click',()=>{
+    navlist.classList.toggle('v-class-resp');
+    rightnav.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h_nav-resp');
+
+})
+
